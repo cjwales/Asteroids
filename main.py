93f81b82269e93bcf44b751ee4285299e8d2b84a
cjwales <cjwales@gmail.com>
2025-12-01 16:55:49 +0000

    add framerate limit and remove prints

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ from logger import log_state
 
 def main():
     pygame.init()
-    print("Starting Asteroids with pygame version: 2.6.1")
-    print("Screen width: 1280")
-    print("Screen height: 720")
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -19,6 +17,8 @@ def main():
         
         screen.fill("black")
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
